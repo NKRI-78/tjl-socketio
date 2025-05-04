26f849c059bd3c41d19f1dc11034e22a5e99cb38
Reihan Agam <reihanagam7@gmail.com>
2025-05-04 22:42:23 +0700

    update

---
+++ app.js
@@ -0,0 +1,64 @@
+require("dotenv").config()
+
+const { uuidv7 } = require('uuidv7')
+
+const { createServer } = require('http')
+const { Server } = require("socket.io")
+
+const express = require('express')
+const moment = require('moment')
+
+const jwt = require('jsonwebtoken')
+const Chat = require("./models/Chat")
+
+const User = require("./models/ApplyJob")
+
+const app = express()
+
+const server = createServer(app)
+
+const io = new Server(server)
+ 
+function setUserId(socket) {
+    let handshake = socket.handshake;
+
+    var authorization = handshake.auth.token;
+
+    if (authorization == undefined) return;
+
+    jwt.verify(authorization, process.env.SECRET_KEY, async function (err, user) {
+        if (!err) {
+            console.log(`User ${user.uid} joined`)
+
+            socket.join(user.uid);
+        }
+    })
+}
+
+io.on('connection', (socket) => {
+    const clientIp = socket.handshake.address;
+    console.log(`[Socket.IO] Client with IP ${clientIp} has connected`);
+
+    setUserId(socket)
+    
+    socket.on('leave', async (message) => {
+        const { user_id } = message;
+
+        socket.leave(user_id);
+    });
+
+    socket.on('apply_job', async (message) => {
+        const { job_id } = message
+
+        const payload = {
+            type: 'apply_job',
+            job_id: job_id
+        };
+
+        socket.emit('apply_job_result', payload);
+    });
+});
+  
+server.listen(process.env.PORT, function () {
+    console.log(`Listening on port ${process.env.PORT}`);
+});
